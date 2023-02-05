bbc7ec6584896e008b39a784c24c3a45d946a2ad
786Shadat <114095300+786Shadat@users.noreply.github.com>
2023-02-05 10:53:29 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".banner_slider").slick({
+        autoplay:true,
+        prevArrow:'<i class="fa-solid fa-chevron-left banner_slider_arrow"></i>',
+        nextArrow:'<i class="fa-solid fa-chevron-right banner_slider_arrow"></i>',
+    })
+}
+)
